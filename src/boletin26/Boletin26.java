a0e3c9213fd9aeb6f64bc53ddde69eee5eb98bcb
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-04-12 10:06:25 +0200

    bonoloto

---
+++ src/boletin26/Boletin26.java
@@ -0,0 +1,11 @@
+package boletin26;
+
+public class Boletin26 {
+
+    public static void main(String[] args) {
+        bonoloto bonoloto = new bonoloto();
+        bonoloto.setVisible(true);
+        
+    }
+    
+}
